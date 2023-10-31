792553a854541c075526931f31ee7fdf63f429e5
Meyers <augustmeyers14@gmail.com>
2023-10-30 22:13:50 -0400

    Updated tests/concurrency

--- cypress/e2e/nav.cy.js
+++ cypress/e2e/nav.cy.js
@@ -1,13 +1,13 @@
 describe('nav view', () => {
   it('default-viewport', () => {
     cy.visit('/')
-    cy.get('.burgerWrapper').should('not.be.visible')
-
+    cy.get('[class*="header_burgerWrapper"]').should('not.be.visible')
+    
   })
   it('iphone-xr-viewport', () => {
     cy.visit('/')
     cy.viewport('iphone-xr')
-    cy.get('.burgerWrapper').should('visible')
+    cy.get('[class*="header_burgerWrapper"]').should('be.visible')
   
   })
 })
