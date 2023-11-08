8e9089e97894dbc7b831f167f141ca198647e1b9
Meyersa <augustmeyers14@gmail.com>
2023-11-08 10:38:13 -0500

    404 page implementation

---
+++ src/pages/404.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+import Header from './components/Header/index.js'
+import Footer from './components/Footer/index.js'
+
+import Image from 'next/image';
+
+export default function Custom404() {
+    return (
+        <div>
+            <Head>
+                <title>Institution Penguin</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <Header />
+            <Image src="/images/habitat.jpeg" alt="404 Habitat not found" width="64" height="64" />
+            <h1>404 - Page Not Found</h1>
+            <Footer />
+        </div>
+    )
+}
