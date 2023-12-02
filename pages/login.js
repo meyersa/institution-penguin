304919bd5343ce7be1f842a1d1595f7246a45096
Jonathan Gregory <95664698+Grego3j@users.noreply.github.com>
2023-12-02 13:18:19 -0500

    Added login logic and assets

--- pages/login.js
+++ pages/login.js
@@ -1,20 +1,47 @@
-import { useSession, signIn, signOut } from "next-auth/react"
+import { useSession, getSession, signIn, signOut } from "next-auth/react"
+import Head from 'next/head'
+import Header from './components/Header/index.js'
+import CenterContent from './components/CenterContent/index.js'
+import Image from 'next/image';
 
-export default function Component() {
-    const { data: session, status: loading } = useSession();
+export default function Login() {
 
-    if (session) {
+    const { data: session, status: status } = useSession();
+    console.log(useSession())
+
+    if (status === "authenticated") {
+        // add login/sign-up logic 
         return (
-            <>
-                Signed in as {session.user.name} <br />
-                <button onClick={() => signOut()}>Sign out</button>
-            </>
+            <div>
+                <Head>
+                    <title>Institution Penguin</title>
+                    <link rel="icon" href="/favicon.ico" />
+                </Head>
+                <Header />
+                <CenterContent>
+                    <Image src="/images/logged-in.png" alt="Logged In" width="1280" height="1280" />
+                    <h1>Signed in as {session.user.name}</h1>
+                    <button onClick={() => signOut()}>Sign out</button>
+                    <button onClick={redirectToGame}>Return to game</button>
+                </CenterContent>
+            </div>
         )
     }
     return (
-        <>
-            Not signed in <br />
-            <button onClick={() => signIn()}>Sign in</button>
-        </>
+        <div>
+            <Head>
+                <title>Institution Penguin</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <Header />
+            <CenterContent>
+                <h1>Not signed in</h1>
+                <button onClick={() => signIn()}>Sign in/Sign up</button>
+            </CenterContent>
+        </div>
     )
+}
+
+function redirectToGame() {
+    location.href = "/404";
 }
