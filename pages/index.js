31b4edcbc960d2db7714627c604eabefef7f3ea9
Jonathan Gregory <95664698+Grego3j@users.noreply.github.com>
2024-02-12 12:19:27 -0500

    Tested some multiplayer stuff

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,21 @@ import Head from 'next/head'
 import Header from './components/Header/index.js'
 import Wrapper from "./components/Wrapper/index.js"
 import Footer from './components/Footer/index.js'
+import { useEffect } from 'react'
+import io from 'socket.io-client'
+let socket
 
 export default function Home() {
+  useEffect(() => { socketInitializer() }, [])
+
+  const socketInitializer = async () => {
+    socket = io("http://localhost:3001")
+
+    socket.on('connect', () => {
+      console.log('connected')
+    })
+  }
+
   return (
     <div>
       <Head>
