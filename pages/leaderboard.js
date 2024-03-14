433b6234229d2e051148157722c14a501f894ae2
meyersa <88554476+meyersa@users.noreply.github.com>
2024-03-13 21:41:21 -0400

    leaderboard rough draft

--- pages/leaderboard.js
+++ pages/leaderboard.js
@@ -15,26 +15,54 @@ export default function Leaderboard() {
             <CenterContent>
                 <div id='boxDisplay'>
                     <div id='boxInside'>
-                        <h1>Top 10</h1>
-                        <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-between", width: "100%", height: "5rem"}}>
-                            <div style={{ display: "flex", flexDirection: "row"}}>
-                                <h2>1st</h2>
+                        <h1>Top 3</h1>
+                        <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-between", width: "100%", height: "4rem"}}>
+                            <div style={{ display: "flex", flexDirection: "row", alignItems: "center"}}>
+                                <h2 style={{minWidth: "3rem"}}>1st</h2>
                                 <Image src="/images/default-avatar.png" width={"100"} height={"100"} alt="Default avatar" style={{width: "100%", height: "100%", maxHeight: "3rem", objectFit: "contain"}}/>
-                                <h2>Lorem ipsum</h2>
+                                <a style={{ minWidth: "10rem"}}>Lorem ipsum</a>
                             </div>
-                            <h2>10858pts</h2>
+                            <h1 style={{fontSize: "1.3rem", alignSelf: "center"}}>10858pts</h1>
                         </div>
-                        <div style={{ display: "flex", flexDirection: "row" }}>
-                            <h2>2st</h2>
-                            <p>pfp</p>
-                            <h2>Lorem ipsum</h2>
-                            <h2>10858 points</h2>
+                        <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-between", width: "100%", height: "4rem"}}>
+                            <div style={{ display: "flex", flexDirection: "row", alignItems: "center"}}>
+                                <h2 style={{minWidth: "3rem"}}>2nd</h2>
+                                <Image src="/images/default-avatar.png" width={"100"} height={"100"} alt="Default avatar" style={{width: "100%", height: "100%", maxHeight: "3rem", objectFit: "contain"}}/>
+                                <a style={{ minWidth: "10rem"}}>Lorem ipsum</a>
+                            </div>
+                            <h1 style={{fontSize: "1.3rem", alignSelf: "center"}}>8501pts</h1>
                         </div>
-                        <div style={{ display: "flex", flexDirection: "row" }}>
-                            <h2>3st</h2>
-                            <p>pfp</p>
-                            <h2>Lorem ipsum</h2>
-                            <h2>10858 points</h2>
+                        <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-between", width: "100%", height: "4rem"}}>
+                            <div style={{ display: "flex", flexDirection: "row", alignItems: "center"}}>
+                                <h2 style={{minWidth: "3rem"}}>3rd</h2>
+                                <Image src="/images/default-avatar.png" width={"100"} height={"100"} alt="Default avatar" style={{width: "100%", height: "100%", maxHeight: "3rem", objectFit: "contain"}}/>
+                                <a style={{ minWidth: "10rem"}}>Lorem ipsum</a>
+                            </div>
+                            <h1 style={{fontSize: "1.3rem", alignSelf: "center"}}>1500pts</h1>
+                        </div>
+                    </div>
+                    <div id='boxInside'>
+                        <h1>Recent Scores</h1>
+                        <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-between", width: "100%", height: "4rem"}}>
+                            <div style={{ display: "flex", flexDirection: "row", alignItems: "center"}}>
+                                <Image src="/images/default-avatar.png" width={"100"} height={"100"} alt="Default avatar" style={{width: "100%", height: "100%", maxHeight: "3rem", objectFit: "contain"}}/>
+                                <a style={{ minWidth: "10rem"}}>Lorem ipsum</a>
+                            </div>
+                            <h1 style={{fontSize: "1.3rem", alignSelf: "center"}}>115pts</h1>
+                        </div>
+                        <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-between", width: "100%", height: "4rem"}}>
+                            <div style={{ display: "flex", flexDirection: "row", alignItems: "center"}}>
+                                <Image src="/images/default-avatar.png" width={"100"} height={"100"} alt="Default avatar" style={{width: "100%", height: "100%", maxHeight: "3rem", objectFit: "contain"}}/>
+                                <a style={{ minWidth: "10rem"}}>Lorem ipsum</a>
+                            </div>
+                            <h1 style={{fontSize: "1.3rem", alignSelf: "center"}}>15pts</h1>
+                        </div>
+                        <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-between", width: "100%", height: "4rem"}}>
+                            <div style={{ display: "flex", flexDirection: "row", alignItems: "center"}}>
+                                <Image src="/images/default-avatar.png" width={"100"} height={"100"} alt="Default avatar" style={{width: "100%", height: "100%", maxHeight: "3rem", objectFit: "contain"}}/>
+                                <a style={{ minWidth: "10rem"}}>Lorem ipsum</a>
+                            </div>
+                            <h1 style={{fontSize: "1.3rem", alignSelf: "center"}}>35pts</h1>
                         </div>
                     </div>
                 </div>
