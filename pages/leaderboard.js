0b8268ddd22be6aa1228af8a35738d32daeb5511
meyersa <88554476+meyersa@users.noreply.github.com>
2024-03-13 21:57:55 -0400

    Finalized template w/ repeating css

--- pages/leaderboard.js
+++ pages/leaderboard.js
@@ -3,6 +3,41 @@ import Header from './components/Header/index.js'
 import Footer from './components/Footer/index.js'
 import CenterContent from './components/CenterContent/index.js'
 import Image from 'next/image.js'
+import Link from 'next/link.js'
+
+const pageCSS = {
+    leaderboardOutside: {
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        width: "100%",
+        height: "4rem"
+    },
+    leaderboardInside: {
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center"
+    },
+    leaderboardPlace: {
+        minWidth: "3rem"
+    },
+    leaderboardImage: {
+        width: "100%",
+        height: "100%",
+        maxHeight: "3rem",
+        objectFit: "contain"
+    },
+    leaderboardLink: {
+        minWidth: "10rem",
+        textDecoration: "underline",
+        textDecorationColor: "rgb(10, 186, 250)",
+        textDecorationThickness: "0.2rem"
+    },
+    leaderboardPoints: {
+        fontSize: "1.3rem",
+        alignSelf: "center"
+    }
+}
 
 export default function Leaderboard() {
     return (
@@ -16,53 +51,53 @@ export default function Leaderboard() {
                 <div id='boxDisplay'>
                     <div id='boxInside'>
                         <h1>Top 3</h1>
-                        <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-between", width: "100%", height: "4rem"}}>
-                            <div style={{ display: "flex", flexDirection: "row", alignItems: "center"}}>
-                                <h2 style={{minWidth: "3rem"}}>1st</h2>
-                                <Image src="/images/default-avatar.png" width={"100"} height={"100"} alt="Default avatar" style={{width: "100%", height: "100%", maxHeight: "3rem", objectFit: "contain"}}/>
-                                <a style={{ minWidth: "10rem"}}>Lorem ipsum</a>
+                        <div style={pageCSS.leaderboardOutside}>
+                            <div style={pageCSS.leaderboardInside}>
+                                <h2 style={pageCSS.leaderboardPlace}>1st</h2>
+                                <Image src="/images/default-avatar.png" width={"100"} height={"100"} alt="Default avatar" style={pageCSS.leaderboardImage} />
+                                <Link href="/profile/lorem-ipsum" style={pageCSS.leaderboardLink}>Lorem ipsum</Link>
                             </div>
-                            <h1 style={{fontSize: "1.3rem", alignSelf: "center"}}>10858pts</h1>
+                            <h1 style={pageCSS.leaderboardPoints}>10858pts</h1>
                         </div>
-                        <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-between", width: "100%", height: "4rem"}}>
-                            <div style={{ display: "flex", flexDirection: "row", alignItems: "center"}}>
-                                <h2 style={{minWidth: "3rem"}}>2nd</h2>
-                                <Image src="/images/default-avatar.png" width={"100"} height={"100"} alt="Default avatar" style={{width: "100%", height: "100%", maxHeight: "3rem", objectFit: "contain"}}/>
-                                <a style={{ minWidth: "10rem"}}>Lorem ipsum</a>
+                        <div style={pageCSS.leaderboardOutside}>
+                            <div style={pageCSS.leaderboardInside}>
+                                <h2 style={pageCSS.leaderboardPlace}>2nd</h2>
+                                <Image src="/images/default-avatar.png" width={"100"} height={"100"} alt="Default avatar" style={pageCSS.leaderboardImage} />
+                                <Link href="/profile/lorem-ipsum" style={pageCSS.leaderboardLink}>Lorem ipsum</Link>
                             </div>
-                            <h1 style={{fontSize: "1.3rem", alignSelf: "center"}}>8501pts</h1>
+                            <h1 style={pageCSS.leaderboardPoints}>9081pts</h1>
                         </div>
-                        <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-between", width: "100%", height: "4rem"}}>
-                            <div style={{ display: "flex", flexDirection: "row", alignItems: "center"}}>
-                                <h2 style={{minWidth: "3rem"}}>3rd</h2>
-                                <Image src="/images/default-avatar.png" width={"100"} height={"100"} alt="Default avatar" style={{width: "100%", height: "100%", maxHeight: "3rem", objectFit: "contain"}}/>
-                                <a style={{ minWidth: "10rem"}}>Lorem ipsum</a>
+                        <div style={pageCSS.leaderboardOutside}>
+                            <div style={pageCSS.leaderboardInside}>
+                                <h2 style={pageCSS.leaderboardPlace}>3rd</h2>
+                                <Image src="/images/default-avatar.png" width={"100"} height={"100"} alt="Default avatar" style={pageCSS.leaderboardImage} />
+                                <Link href="/profile/lorem-ipsum" style={pageCSS.leaderboardLink}>Lorem ipsum</Link>
                             </div>
-                            <h1 style={{fontSize: "1.3rem", alignSelf: "center"}}>1500pts</h1>
+                            <h1 style={pageCSS.leaderboardPoints}>1500pts</h1>
                         </div>
                     </div>
                     <div id='boxInside'>
                         <h1>Recent Scores</h1>
-                        <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-between", width: "100%", height: "4rem"}}>
-                            <div style={{ display: "flex", flexDirection: "row", alignItems: "center"}}>
-                                <Image src="/images/default-avatar.png" width={"100"} height={"100"} alt="Default avatar" style={{width: "100%", height: "100%", maxHeight: "3rem", objectFit: "contain"}}/>
-                                <a style={{ minWidth: "10rem"}}>Lorem ipsum</a>
+                        <div style={pageCSS.leaderboardOutside}>
+                            <div style={pageCSS.leaderboardInside}>
+                                <Image src="/images/default-avatar.png" width={"100"} height={"100"} alt="Default avatar" style={pageCSS.leaderboardImage} />
+                                <Link href="/profile/lorem-ipsum" style={pageCSS.leaderboardLink}>Lorem ipsum</Link>
                             </div>
-                            <h1 style={{fontSize: "1.3rem", alignSelf: "center"}}>115pts</h1>
+                            <h1 style={pageCSS.leaderboardPoints}>108pts</h1>
                         </div>
-                        <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-between", width: "100%", height: "4rem"}}>
-                            <div style={{ display: "flex", flexDirection: "row", alignItems: "center"}}>
-                                <Image src="/images/default-avatar.png" width={"100"} height={"100"} alt="Default avatar" style={{width: "100%", height: "100%", maxHeight: "3rem", objectFit: "contain"}}/>
-                                <a style={{ minWidth: "10rem"}}>Lorem ipsum</a>
+                        <div style={pageCSS.leaderboardOutside}>
+                            <div style={pageCSS.leaderboardInside}>
+                                <Image src="/images/default-avatar.png" width={"100"} height={"100"} alt="Default avatar" style={pageCSS.leaderboardImage} />
+                                <Link href="/profile/lorem-ipsum" style={pageCSS.leaderboardLink}>Lorem ipsum</Link>
                             </div>
-                            <h1 style={{fontSize: "1.3rem", alignSelf: "center"}}>15pts</h1>
+                            <h1 style={pageCSS.leaderboardPoints}>858pts</h1>
                         </div>
-                        <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-between", width: "100%", height: "4rem"}}>
-                            <div style={{ display: "flex", flexDirection: "row", alignItems: "center"}}>
-                                <Image src="/images/default-avatar.png" width={"100"} height={"100"} alt="Default avatar" style={{width: "100%", height: "100%", maxHeight: "3rem", objectFit: "contain"}}/>
-                                <a style={{ minWidth: "10rem"}}>Lorem ipsum</a>
+                        <div style={pageCSS.leaderboardOutside}>
+                            <div style={pageCSS.leaderboardInside}>
+                                <Image src="/images/default-avatar.png" width={"100"} height={"100"} alt="Default avatar" style={pageCSS.leaderboardImage} />
+                                <Link href="/profile/lorem-ipsum" style={pageCSS.leaderboardLink}>Lorem ipsum</Link>
                             </div>
-                            <h1 style={{fontSize: "1.3rem", alignSelf: "center"}}>35pts</h1>
+                            <h1 style={pageCSS.leaderboardPoints}>108pts</h1>
                         </div>
                     </div>
                 </div>
