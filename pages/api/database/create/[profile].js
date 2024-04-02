13a2ee45c632dd3614b12259b00bf861bb17d8c3
meyersa <88554476+meyersa@users.noreply.github.com>
2024-04-02 19:45:21 -0400

    Future mockup

---
+++ pages/api/database/create/[profile].js
@@ -0,0 +1,6 @@
+/*
+ * Create profile API endpoint
+ * When a new user logs in this should be fetched 
+ * Requires NextAuth session 
+ * Creates profile with an incremented ID and either user input or Github pass in
+ */ 
