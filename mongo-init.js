55437ad9918cf8fa791f0a66679c3b1b8d579ce4
meyersa <88554476+meyersa@users.noreply.github.com>
2024-04-02 17:46:00 -0400

    More test data

--- mongo-init.js
+++ mongo-init.js
@@ -6,18 +6,51 @@ db.createCollection("players")
 db.createCollection("scores")
 
 // Insert test data
-db.players.insertOne({
-    "id": 1,
-    "score": 100,
-    "email": "example@email.com",
-    "displayName": "Lorem Ipsum",
-    "creationDate": new Date(),
-    "lastActivityDate": new Date()
-})
-
-db.scores.insertOne({
-    "value": 100,
-    "playerID": 1,
-    "gameName": "flappypenguin",
-    "timestamp": new Date()
-})
+if (process.env.ENVIRONMENT == "DEV") {
+    db.players.insertMany([
+        {
+            "id": 1,
+            "totalScore": 100,
+            "email": "example@email.com",
+            "displayName": "Lorem Ipsum",
+            "creationDate": new Date(),
+            "lastActivityDate": new Date()
+        },
+        {
+            "id": 2,
+            "totalScore": 200,
+            "email": "example2@email.com",
+            "displayName": "Dolor Sit",
+            "creationDate": new Date(),
+            "lastActivityDate": new Date()
+        },
+        {
+            "id": 3,
+            "totalScore": 150,
+            "email": "example3@email.com",
+            "displayName": "Amet Consectetur",
+            "creationDate": new Date(),
+            "lastActivityDate": new Date()
+        },
+    ]);
+    db.scores.insertMany([
+        {
+            "value": 100,
+            "playerID": 1,
+            "gameName": "flappypenguin",
+            "timestamp": new Date()
+        },
+        {
+            "value": 200,
+            "playerID": 2,
+            "gameName": "flappypenguin",
+            "timestamp": new Date()
+        },
+        {
+            "value": 150,
+            "playerID": 3,
+            "gameName": "flappypenguin",
+            "timestamp": new Date()
+        },
+    ]);
+}
