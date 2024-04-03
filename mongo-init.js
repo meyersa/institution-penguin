bf64a1123b6362897624d7dd2519cd540c5bca3d
meyersa <88554476+meyersa@users.noreply.github.com>
2024-04-03 09:53:46 -0400

    Add default data for live as well

--- mongo-init.js
+++ mongo-init.js
@@ -6,24 +6,43 @@ db.createCollection("players")
 db.createCollection("scores")
 
 // Insert test data
+db.players.insertOne(
+    {
+        "playerID": 1,
+        "email": "admin@institutionpenguin.com",
+        "displayName": "OG Penguin",
+        "creationDate": new Date('Wed Apr 03 2024 09:37:10 GMT-0400 (Eastern Daylight Time)'),
+        "lastActivityDate": new Date('Wed Apr 03 2024 09:37:10 GMT-0400 (Eastern Daylight Time)')
+    },
+)
+db.scores.insertMany([
+    {
+        "value": 100,
+        "playerID": 1,
+        "gameName": "flappypenguin",
+        "timestamp": new Date('Wed Apr 03 2024 09:37:10 GMT-0400 (Eastern Daylight Time)')
+    }
+]);
+
+// More data for Dev environment
 if (process.env.ENVIRONMENT == "DEV") {
     db.players.insertMany([
         {
-            "playerID": 1,
+            "playerID": 2,
             "email": "example@email.com",
             "displayName": "Lorem Ipsum",
             "creationDate": new Date(),
             "lastActivityDate": new Date()
         },
         {
-            "playerID": 2,
+            "playerID": 3,
             "email": "example2@email.com",
             "displayName": "Dolor Sit",
             "creationDate": new Date(),
             "lastActivityDate": new Date()
         },
         {
-            "playerID": 3,
+            "playerID": 4,
             "email": "example3@email.com",
             "displayName": "Amet Consectetur",
             "creationDate": new Date(),
@@ -33,25 +52,25 @@ if (process.env.ENVIRONMENT == "DEV") {
     db.scores.insertMany([
         {
             "value": 100,
-            "playerID": 1,
+            "playerID": 2,
             "gameName": "flappypenguin",
             "timestamp": new Date()
         },
         {
             "value": 200,
-            "playerID": 2,
-            "gameName": "flappypenguin",
-            "timestamp": new Date()
-        },
-        {
-            "value": 150,
             "playerID": 3,
             "gameName": "flappypenguin",
             "timestamp": new Date()
         },
         {
+            "value": 150,
+            "playerID": 4,
+            "gameName": "flappypenguin",
+            "timestamp": new Date()
+        },
+        {
             "value": 250,
-            "playerID": 2,
+            "playerID": 3,
             "gameName": "triviapenguin",
             "timestamp": new Date()
         },
