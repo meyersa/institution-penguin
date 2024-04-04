db2a3b3f63418e7e088efd4ab18e5404ab89309d
meyersa <88554476+meyersa@users.noreply.github.com>
2024-04-04 01:03:35 -0400

    Bool instead of string

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,6 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
     e2e: {
         // Since there are weird script artifacts
-        testIsolation: 'false',
+        testIsolation: false,
       },
 })
