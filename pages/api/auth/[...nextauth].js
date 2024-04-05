92d670a1e7e4bc9cc7f5658334a22674a134d85b
meyersa <88554476+meyersa@users.noreply.github.com>
2024-04-05 12:45:29 -0400

    NextAuth create user/update user

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,15 +1,75 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import { MongoClient } from 'mongodb';
 
-export const authOptions = {
-    // Configure one or more authentication providers
+export default NextAuth({
     providers: [
         GithubProvider({
             clientId: process.env.AUTH_ID,
             clientSecret: process.env.AUTH_SECRET,
         }),
     ],
-    secret: process.env.NEXTAUTH_SECRET
-}
+    secret: process.env.NEXTAUTH_SECRET,
+    callbacks: {
+        async signIn(user) {
 
-export default NextAuth(authOptions)
+            // Store user data in MongoDB
+            try {
+
+                // Get information from signIn
+                const authUsername = user.user.name 
+                const authEmail = user.user.email 
+                const authPic = user.user.image
+
+                console.log(authUsername + " " + authEmail + " " + authPic)
+
+                // Get credentials from ENV
+                const { MONGODB_URL, MONGODB_DB } = process.env;
+
+                // Connect to Mongo
+                const client = await MongoClient.connect(MONGODB_URL);
+
+                // Connect to specified DB
+                const db = await client.db(MONGODB_DB);
+                const players = db.collection('players')
+
+                // Check if user already exists in the database
+                const existingUser = await players.findOne({ email: authEmail });
+                const nextPlayerID = players.find().sort({ "playerID": -1 }).limit(1) + 1;
+                
+                if (!existingUser) {
+
+                    // Create a new user profile in MongoDB
+                    await players.insertOne({
+                        playerID: nextPlayerID,
+                        email: authEmail,
+                        displayName: authUsername,
+                        profilePic: authPic,
+                        creationDate: new Date(),
+                        lastActivityDate: new Date(),
+
+                    }).then(
+                        console.log(`Created user ${authUsername} with the email ${authEmail}`)
+
+                    );
+                } else {
+
+                    // Update recent date for existing user
+                    players.updateOne(
+                        { email: authEmail },
+                        { $set: { lastActivityDate: new Date() } }
+
+                    ).then(
+                        console.log(`Updated user login for ${authEmail}`)
+
+                    )
+                }
+            } catch (error) {
+                console.error('Error storing user data:', error);
+
+            }
+            return true;
+
+        },
+    },
+});
--- pages/api/database/create/[profile].js
+++
@@ -1,6 +0,0 @@
-/*
- * Create profile API endpoint
- * When a new user logs in this should be fetched 
- * Requires NextAuth session 
- * Creates profile with an incremented ID and either user input or Github pass in
- */ 
