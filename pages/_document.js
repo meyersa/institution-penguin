3cb45732b1fa54fb1e2f08ea83e6b872e79f34b2
meyersa <88554476+meyersa@users.noreply.github.com>
2024-04-07 20:05:19 -0400

    Header that reflects session information

--- pages/_document.js
+++ pages/_document.js
@@ -14,6 +14,7 @@ export default function Document() {
                 {/* Global font sheets */}
                 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=ABeeZee&display=swap"></link>
                 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Sedgwick+Ave+Display&display=swap"></link>
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
             </Head>
             <body>
                 <Main />
