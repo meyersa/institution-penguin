c7fe38aa4c4f8344bac6c218ebf345c8024d188b
meyersa <88554476+meyersa@users.noreply.github.com>
2024-04-08 15:08:08 -0400

    New mobile menu

--- pages/components/Header/index.js
+++ pages/components/Header/index.js
@@ -23,57 +23,81 @@ export default function Header() {
   // Push /login on desktop and open menu on mobile
   function handleButtonClick() {
     // Push to login on desktop
-    if (window.innerWidth > 480) {
+    if (window.innerWidth > 740) {
       window.location.href = "/login";
       return;
     }
 
     // Display menu
     if (!menuDisplayed) {
-      document.getElementById("mobileMenu").style.top = "6em";
+      document.getElementById("mobileMenu").style.transform = "translateY(calc(0% + 6em))";
+      document.body.style.height = "100%";
+      document.body.style.overflow = "hidden";
       setDisplayed(true);
       return;
     }
 
     // Hide menu
-    document.getElementById("mobileMenu").style.top = "100%";
+    document.getElementById("mobileMenu").style.transform = "translateY(-100%)";
+    document.body.style.height = "initial";
+    document.body.style.overflow = "initial";
     setDisplayed(false);
+    return;
+  }
+
+  function handleMobileRedirect(redirectPage) {
+    if (window.location.pathname == redirectPage) {
+      handleButtonClick();
+      return;
+    }
+
+    document.body.style.height = "initial";
+    document.body.style.overflow = "initial";
+    window.location.href = redirectPage;
+    setDisplayed(false);
+    return;
   }
 
   return (
-    <div className={styles.headerbar}>
-      <div className={styles.leftheader}>
-        <Link className={styles.inside} href="/">
-          institution penguin
-        </Link>
-      </div>
-      <div className={styles.rightheader}>
-        <Link className={styles.inside} href="/about">
-          About
-        </Link>
-        <Link className={styles.inside} href="/leaderboard">
-          Leaderboard
-        </Link>
-        <button id="profileButton" className={styles.profileButton} onClick={handleButtonClick}>
-          {loggedIn ? (
-            <Image className={styles.headerImage} src={profPicture} height={50} width={50} alt="Profile picture" />
-          ) : (
-            <i className={["fa", "fa-bars", styles.FABars].join(" ")} />
-          )}
-          <a className={styles.headerText}>{displayText}</a>
-        </button>
-      </div>
-      <div id="mobileMenu" className={styles.mobileMenu}>
-        <div className={styles.mobileTop}>
-          <Link id="about" className={styles.menuitem} href="/about">
+    <div className={styles.headerOutside} id="headerOutside">
+      <div className={styles.headerbar}>
+        <div className={styles.leftheader}>
+          <Link className={styles.inside} href="/">
+            institution penguin
+          </Link>
+        </div>
+        <div className={styles.rightheader}>
+          <Link className={styles.inside} href="/about">
             About
           </Link>
-          <Link id="leaderboard" className={styles.menuitem} href="/leaderboard">
+          <Link className={styles.inside} href="/leaderboard">
             Leaderboard
           </Link>
-          <Link className={styles.menuitem} href="/login">
+          <button id="profileButton" className={styles.profileButton} onClick={() => handleButtonClick()}>
+            {loggedIn ? (
+              <Image className={styles.headerImage} src={profPicture} height={50} width={50} alt="Profile picture" />
+            ) : (
+              <i className={["fa", "fa-bars", styles.FABars].join(" ")} />
+            )}
+            <a className={styles.headerText}>{displayText}</a>
+          </button>
+        </div>
+      </div>
+      <div id="mobileMenu" className={styles.mobileMenu}>
+        <img className={styles.mobilePenguin} src="/images/penguin-2.svg" alt="Penguin sliding in" />
+        <div className={styles.mobileTop}>
+          <button className={styles.menuitem} onClick={() => handleMobileRedirect("/about")}>
+            About
+          </button>
+          <button className={styles.menuitem} onClick={() => handleMobileRedirect("/leaderboard")}>
+            Leaderboard
+          </button>
+          <button className={styles.menuitem} onClick={() => handleMobileRedirect("/login")}>
             {loggedIn ? "Logout" : "Login"}
-          </Link>
+          </button>
+          <button className={styles.menuexit} onClick={() => handleButtonClick()}>
+            X
+          </button>
         </div>
         <a className={styles.menuitemsmall}>&copy; InstitutionPenguin.com {new Date().getFullYear()}</a>
       </div>
