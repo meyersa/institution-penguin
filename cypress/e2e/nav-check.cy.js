679ed335a3627743c3078f629169bcfd91542d0f
meyersa <88554476+meyersa@users.noreply.github.com>
2024-04-07 20:12:15 -0400

    Fix test to include button/new IDs

--- cypress/e2e/nav-check.cy.js
+++ cypress/e2e/nav-check.cy.js
@@ -6,12 +6,18 @@ describe('nav view', () => {
   });
 
   it('default-viewport should not show the burger menu', () => {
-    cy.get('[class*="header_burgerWrapper"]').should('not.be.visible')
+    cy.get('[class*="mobile"]').should('not.be.visible')
 
   });
-  it('iphone-xr-viewport should show the burger menu', () => {
+  it('iphone-xr-viewport should only show the burger menu when pressed', () => {
     cy.viewport('iphone-xr')
-    cy.get('[class*="header_burgerWrapper"]').should('be.visible')
+    cy.get('[class*="header_mobileMenu"]').should('not.be.visible')
+
+  });
+  it('iphone-xr-viewport should show the burger menu when pressed', () => {
+    cy.viewport('iphone-xr')
+    cy.get('#profileButton').click();
+    cy.get('[class*="header_mobileMenu"]').should('be.visible')
 
   });
 })
