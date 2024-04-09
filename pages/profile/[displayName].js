4f5354e372d03ebae6798ca2a536931389b551b2
meyersa <88554476+meyersa@users.noreply.github.com>
2024-04-09 15:32:58 -0400

    Move profile over to direct queries/handle null route

---
+++ pages/profile/[displayName].js
@@ -0,0 +1,124 @@
+import Head from 'next/head'
+import Header from '../components/Header/index.js'
+import Footer from '../components/Footer/index.js'
+import CenterContent from '../components/CenterContent/index.js'
+import Image from 'next/image.js'
+import { formatRelativeDate } from '../../util/time.js'
+import { profileInfo } from '../../util/mongoBackendQuery.js'
+
+const pageCSS = {
+    profileTop: {
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        width: "100%",
+    },
+    profileLeft: {
+        display: "flex",
+        flexDirection: "column",
+
+    },
+    paddedBox: {
+        marginTop: "0.25rem",
+        marginBottom: "0.25rem",
+        minWidth: "10rem",
+        display: "block"
+    },
+    profileImage: {
+        height: "100%",
+        maxHeight: "10rem",
+        maxWidth: "10rem",
+        objectFit: "contain"
+
+    },
+    recentScore: {
+        display: "flex",
+        width: "100%",
+        alignItems: "center",
+        justifyContent: "space-between",
+
+    },
+    points: {
+        fontSize: "1.3rem",
+
+    },
+}
+
+export default function Profile({ playerProfile }) {
+    return (
+        <div>
+            <Head>
+                <title>Institution Penguin</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <Header />
+            <CenterContent>
+                <div id='boxDisplay'>
+                    <div id='boxInside'>
+                        <div style={pageCSS.profileTop}>
+                            <div style={pageCSS.profileLeft}>
+                                <h1>{playerProfile.playerInfo.displayName}</h1>
+                                <a style={pageCSS.paddedBox}>Total points: {playerProfile.globalScore}</a>
+                                <a style={pageCSS.paddedBox}>Global rank: {playerProfile.globalRank}</a>
+                                <a style={pageCSS.paddedBox}>Last active: {formatRelativeDate(playerProfile.playerInfo.lastActivityDate)}</a>
+                                <a style={pageCSS.paddedBox}>Member since: {formatRelativeDate(playerProfile.playerInfo.creationDate)}</a>
+                            </div>
+                            <Image src="/images/default-avatar.png" width={"100"} height={"100"} alt="Default avatar" style={pageCSS.profileImage} />
+                        </div>
+                    </div>
+                    <div id='boxInside'>
+                        <h1>Recent Activity</h1>
+                        {/* Conditional rendering of recent scores */}
+                        {playerProfile.recentScores && playerProfile.recentScores.length > 0 ? (
+                            playerProfile.recentScores.slice(0, 3).map((res, index) => (
+                                <div style={pageCSS.recentScore} key={index}>
+                                    {/* Score information */}
+                                    <a style={pageCSS.paddedBox}>{res.gameName} {formatRelativeDate(res.timestamp)}</a>
+                                    {/* Score value */}
+                                    <h1 style={pageCSS.points}>{res.value}pts</h1>
+                                </div>
+                            ))
+                        ) : (
+                            // Display a message if no recent scores exist
+                            <a>No recent activity</a>
+                        )}
+                    </div>
+                </div>
+            </CenterContent>
+            <Footer />
+        </div>
+    )
+}
+
+export async function getServerSideProps({ query }) {
+    const displayName = query.displayName;
+    let playerProfile = null;
+
+    try {
+        // Query player result
+        let playerRes = await profileInfo(displayName);
+        if (playerRes == undefined) {
+            throw new Error("Profile not found for given displayName")
+
+        }
+        playerProfile = JSON.parse(JSON.stringify(playerRes));
+
+    } catch (e) {
+        // If profile doesn't exist, return 404 
+        return {
+            redirect: {
+                destination: '/404',
+                permanent: false,
+            },
+        }
+    }
+
+    // Pass fetched playerProfile as props
+    return {
+        props: {
+            playerProfile,
+
+        },
+    };
+}
--- util/mongoBackendQuery.js
+++ util/mongoBackendQuery.js
@@ -1,131 +1,204 @@
 import ipCollection from "./mongoInit";
 
 export async function topPlayers() {
-    const queryRes = await ipCollection
-        .collection("scores")
-        .aggregate([
-            {
-                $group: {
-                    _id: "$displayName",
-                    totalScore: { $sum: "$value" }, // Calculate total score for each player
-                },
-            },
-            {
-                $lookup: {
-                    from: "players",
-                    localField: "_id",
-                    foreignField: "displayName",
-                    as: "playerInfo",
-                },
-            },
-            {
-                $unwind: "$playerInfo",
-            },
-            {
-                $sort: { totalScore: -1 },
-            },
-            {
-                $limit: 3,
-            },
-            {
-                $project: {
-                    _id: 0,
-                    displayName: "$playerInfo.displayName",
-                    lastActivityDate: "$playerInfo.lastActivityDate",
-                    totalScore: 1,
-                },
-            },
-        ])
-        .toArray();
+  const queryRes = await ipCollection
+    .collection("scores")
+    .aggregate([
+      {
+        $group: {
+          _id: "$displayName",
+          totalScore: { $sum: "$value" }, // Calculate total score for each player
+        },
+      },
+      {
+        $lookup: {
+          from: "players",
+          localField: "_id",
+          foreignField: "displayName",
+          as: "playerInfo",
+        },
+      },
+      {
+        $unwind: "$playerInfo",
+      },
+      {
+        $sort: { totalScore: -1 },
+      },
+      {
+        $limit: 3,
+      },
+      {
+        $project: {
+          _id: 0,
+          displayName: "$playerInfo.displayName",
+          lastActivityDate: "$playerInfo.lastActivityDate",
+          totalScore: 1,
+        },
+      },
+    ])
+    .toArray();
 
-    return queryRes;
+  return queryRes;
 }
 
 export async function highScores() {
-    const queryRes = await ipCollection
-        .collection("scores")
-        .aggregate([
-            {
-                $group: {
-                    _id: { displayName: "$displayName", gameName: "$gameName" },
-                    maxScore: { $max: "$value" },
-                    timestamp: { $first: "$timestamp" }, // Include timestamp field
-                },
+  const queryRes = await ipCollection
+    .collection("scores")
+    .aggregate([
+      {
+        $group: {
+          _id: { displayName: "$displayName", gameName: "$gameName" },
+          maxScore: { $max: "$value" },
+          timestamp: { $first: "$timestamp" }, // Include timestamp field
+        },
+      },
+      { $sort: { maxScore: -1 } },
+      {
+        $project: {
+          _id: 0,
+          displayName: "$_id.displayName",
+          gameName: "$_id.gameName",
+          maxScore: 1,
+          timestamp: 1, // Project timestamp field
+        },
+      },
+      {
+        $group: {
+          _id: null,
+          topScores: {
+            $push: {
+              displayName: "$displayName",
+              gameName: "$gameName",
+              maxScore: "$maxScore",
+              timestamp: "$timestamp",
             },
-            { $sort: { maxScore: -1 } },
-            {
-                $project: {
-                    _id: 0,
-                    displayName: "$_id.displayName",
-                    gameName: "$_id.gameName",
-                    maxScore: 1,
-                    timestamp: 1, // Project timestamp field
-                },
-            },
-            {
-                $group: {
-                    _id: null,
-                    topScores: {
-                        $push: {
-                            displayName: "$displayName",
-                            gameName: "$gameName",
-                            maxScore: "$maxScore",
-                            timestamp: "$timestamp",
-                        },
-                    },
-                },
-            },
-            { $unwind: "$topScores" },
-            { $limit: 3 },
-            {
-                $lookup: {
-                    from: "players",
-                    localField: "topScores.displayName",
-                    foreignField: "displayName",
-                    as: "playerInfo",
-                },
-            },
-            { $unwind: "$playerInfo" },
-            {
-                $project: {
-                    _id: 0,
-                    displayName: "$topScores.displayName",
-                    gameName: "$topScores.gameName",
-                    maxScore: "$topScores.maxScore",
-                    displayName: "$playerInfo.displayName",
-                    timestamp: "$topScores.timestamp",
-                },
-            },
-        ])
-        .toArray();
+          },
+        },
+      },
+      { $unwind: "$topScores" },
+      { $limit: 3 },
+      {
+        $lookup: {
+          from: "players",
+          localField: "topScores.displayName",
+          foreignField: "displayName",
+          as: "playerInfo",
+        },
+      },
+      { $unwind: "$playerInfo" },
+      {
+        $project: {
+          _id: 0,
+          displayName: "$topScores.displayName",
+          gameName: "$topScores.gameName",
+          maxScore: "$topScores.maxScore",
+          displayName: "$playerInfo.displayName",
+          timestamp: "$topScores.timestamp",
+        },
+      },
+    ])
+    .toArray();
 
-    return queryRes;
+  return queryRes;
 }
 
 export async function recentScores() {
-    const queryRes = await ipCollection.collection('scores').aggregate([
-        { $sort: { timestamp: -1 } }, // Sort by timestamp in descending order
-        { $limit: 3 }, // Limit to the top 3 scores
-        {
-            $lookup: {
-                from: "players",
-                localField: "displayName",
-                foreignField: "displayName",
-                as: "playerInfo"
-            }
+  const queryRes = await ipCollection
+    .collection("scores")
+    .aggregate([
+      { $sort: { timestamp: -1 } }, // Sort by timestamp in descending order
+      { $limit: 3 }, // Limit to the top 3 scores
+      {
+        $lookup: {
+          from: "players",
+          localField: "displayName",
+          foreignField: "displayName",
+          as: "playerInfo",
         },
-        { $unwind: "$playerInfo" }, // Unwind the playerInfo array
-        {
-            $project: {
-                _id: 0,
-                displayName: "$displayName", // Prefix with $ to reference field from input document
-                gameName: "$gameName", // Prefix with $ to reference field from input document
-                score: "$value", // Prefix with $ to reference field from input document
-                displayName: "$playerInfo.displayName", // Prefix with $ to reference field from embedded document
-                timestamp: "$timestamp",
-            }
+      },
+      { $unwind: "$playerInfo" }, // Unwind the playerInfo array
+      {
+        $project: {
+          _id: 0,
+          displayName: "$displayName", // Prefix with $ to reference field from input document
+          gameName: "$gameName", // Prefix with $ to reference field from input document
+          score: "$value", // Prefix with $ to reference field from input document
+          displayName: "$playerInfo.displayName", // Prefix with $ to reference field from embedded document
+          timestamp: "$timestamp",
         },
-    ]).toArray();
+      },
+    ])
+    .toArray();
 
-    return queryRes
+  return queryRes;
+}
+
+export async function profileInfo(displayName) {
+  // Query MongoDB with findOne using the parsed displayName
+  const playerInfo = await ipCollection.collection("players").findOne(
+    { displayName: displayName },
+    {
+      projection: {
+        _id: 0, // Exclude the _id field
+        displayName: 1,
+        creationDate: 1,
+        lastActivityDate: 1,
+      },
+    }
+  );
+
+  // If there is no player
+  if (!playerInfo) {
+    return;
+  }
+
+  // Calculate global rank
+  const globalRank =
+    (await ipCollection.collection("players").countDocuments({
+      totalScore: { $gt: playerInfo.totalScore },
+    })) + 1;
+
+  // Calculate global score from all game scores
+  const globalScoreAggregate = await ipCollection
+    .collection("scores")
+    .aggregate([
+      { $match: { displayName: displayName } }, // Filter by displayName
+      { $group: { _id: null, totalScore: { $sum: "$value" } } }, // Calculate total score
+    ])
+    .toArray();
+  const globalScore = globalScoreAggregate.length ? globalScoreAggregate[0].totalScore : 0;
+
+  // Find their recent scores
+  const recentScores = await ipCollection
+    .collection("scores")
+    .aggregate([
+      {
+        $match: { displayName: displayName }, // Filter by displayName
+      },
+      {
+        $sort: { timestamp: -1 }, // Sort by timestamp in descending order
+      },
+      {
+        $limit: 3, // Limit to the last 3 scores
+      },
+      {
+        $project: {
+          _id: 0,
+          value: 1,
+          gameName: 1,
+          timestamp: 1,
+        },
+      },
+    ])
+    .toArray();
+
+  // Combine the results
+  const combinedResult = {
+    playerInfo,
+    globalRank,
+    globalScore,
+    recentScores,
+  };
+
+  return combinedResult;
 }
